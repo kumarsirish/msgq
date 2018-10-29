7ae84dbbf7e51305c2fca76c4dd2371ac6c721e0
Sirish Kumar <sirish.kumar@oracle.com>
2018-10-29 15:27:41 +0530

    pacckage name changed

--- commonlib/commonlib.go
+++ commonlib/commonlib.go
@@ -1,10 +1,10 @@
-package libs
+package commonlib
 import (
   "log"
   "fmt"
   )
 
-func failOnError(err error, msgerr string, msgsuc string) {
+func FailOnError(err error, msgerr string, msgsuc string) {
         if err != nil {
                 log.Fatalf("%s: %s",msgerr,err);
                 
